8c3dc81c4feb6911dc5adedbe93d07de006cf603
Cherguelaine Oussama <lo_cherguelaine@esi.dz>
2024-06-29 12:22:59 +0200

    finalisation of download page (desktop - mobile)

--- optimum-website/src/main.jsx
+++ optimum-website/src/main.jsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client'
 // import Footer from './homepage/Components/Footer.jsx'
 import './index.css'
 import AboutPage from './about_page/about_page.jsx'
+import Download from './download_page/download.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Navbar/> */}
     <AboutPage />
     {/* <Footer/> */}
+    <Download/>
   </React.StrictMode>,
 )
