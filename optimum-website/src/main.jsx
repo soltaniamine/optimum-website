8efb7642e0ade78f75004642af7cf0cd52764085
soltaniamine <lm_soltani@esi.dz>
2024-06-29 19:08:18 +0100

    Navbar linked with components

--- optimum-website/src/main.jsx
+++ optimum-website/src/main.jsx
@@ -5,7 +5,6 @@ import Footer from './homepage/Components/Footer.jsx'
 import Home from './homepage/Home/Home.jsx'
 import Separateur from './homepage/Home/Separateur.jsx'
 import Features from './homepage/Features/Features.jsx'
-import Null from './homepage/Components/Null.jsx'
 import './index.css'
 import AboutPage from './about_page/about_page.jsx'
 import Download from './download_page/download.jsx'
@@ -16,12 +15,24 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <body className="custom-scrollbar">
       <Navbar />
       <main className="min-h-screen">
-        <Home/>
-        <Separateur/>
-        <AboutPage />
-        <Features/>
-        <Feedbacks />
-        <Download/>
+        <div id="home">
+          <Home />
+        </div>
+        <div>
+          <Separateur/>
+        </div>
+        <div id="About">
+          <AboutPage />
+        </div>
+        <div id="Features">
+          <Features/> 
+        </div>
+        <div id="Aboutus">
+          <Feedbacks />
+        </div>
+        <div id="App">
+          <Download/>
+        </div>
       </main>
       <Footer/>
     </body>
