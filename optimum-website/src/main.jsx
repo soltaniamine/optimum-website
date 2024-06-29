27542e5e5128861bcab391cac7fe45388cd27aeb
Cherguelaine Oussama <lo_cherguelaine@esi.dz>
2024-06-29 13:14:50 +0200

    finalisation of feedback page (desktop-mobile)

--- optimum-website/src/main.jsx
+++ optimum-website/src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import Navbar from './homepage/Components/Navbar.jsx'
-// import Footer from './homepage/Components/Footer.jsx'
+import Navbar from './homepage/Components/Navbar.jsx'
+import Footer from './homepage/Components/Footer.jsx'
 import './index.css'
 import AboutPage from './about_page/about_page.jsx'
 import Download from './download_page/download.jsx'
+import Feedbacks from './feedbacks_page/feedbacks.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Navbar/> */}
+    <Navbar/>
     <AboutPage />
-    {/* <Footer/> */}
+    <Feedbacks />
     <Download/>
+    <Footer/>
   </React.StrictMode>,
 )
